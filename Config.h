59d4ebe28c3cae00fb5905acda5d14b553901911
Nello.C <nello.chom@protonmail.com>
2022-12-29 12:53:30 +0100

    Remove unused struct

--- Config.h
+++ Config.h
@@ -5,11 +5,4 @@
 #define ALLOCATION_SIZE 1024
 #define BLOCK_SIZE 128
 
-typedef struct
-{
-    uint8_t * pu8FileStream;
-    uint32_t u32FileSize;
-    uint32_t u32Cursor;
-} TsMyFile;
-
 #endif // CONFIG_H_INCLUDED
