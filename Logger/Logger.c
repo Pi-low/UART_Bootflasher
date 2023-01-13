d5ec303a54e92f4509b8a39bbf91827b49506f41
Nello.C <nello.chom@protonmail.com>
2023-01-13 14:51:37 +0100

    log activity to unique txt file

--- Logger/Logger.c
+++ Logger/Logger.c
@@ -10,18 +10,53 @@ FILE * LogStream = NULL;
 
 void Logger_Init(void)
 {
-    LogStream = fopen("log.txt", "w+");
+    char pcDateTime[40];
+    char pcFileName[40];
+    time_t tsTime = time(NULL);
+    struct tm *pTime = localtime(&tsTime);
+
+    strftime(pcDateTime, 20, "%Y-%m-%d_%H.%M.%S", pTime);
+    sprintf(pcFileName, "%s_log.txt", pcDateTime);
+
+    LogStream = fopen(pcFileName, "w+");
+
     if (LogStream != NULL)
     {
-        fprintf(LogStream, "%s %s\r\nLog Session start\r\n", __DATE__, __TIME__);
+        fprintf(LogStream, "<%s>: Bootloader session start\r\n", pcDateTime);
     }
 }
 
-void Logger_Log(char* FpcString)
+void Logger_Append(char *FpcString)
 {
-    if (LogStream != NULL)
+    char pcDateTime[20];
+    time_t tsTime = time(NULL);
+    struct tm *pTime = localtime(&tsTime);
+    strftime(pcDateTime, 20, "%H:%M:%S", pTime);
+
+    if (LogStream != NULL && FpcString != NULL)
     {
-        fprintf(LogStream, "%s\r\n", FpcString);
+        fprintf(LogStream, "<%s> %s", pcDateTime, FpcString);
+    }
+}
+
+void Logger_AppendArray(char *FpcText, uint8_t *Fpu8Array, uint16_t Fu16Len)
+{
+    uint16_t u16Index = 0;
+    char pcSmallString[20];
+
+    if ((LogStream != NULL) && (Fpu8Array != NULL) && (FpcText!= NULL))
+    {
+        for (u16Index = 0; u16Index < Fu16Len; u16Index++)
+        {
+            if ((u16Index != 0) && ((u16Index % LOG_GROUP_BYTE_PER_LINE) == 0))
+            {
+                sprintf(pcSmallString, "\r\n");
+                strcat(FpcText, pcSmallString);
+            }
+            sprintf(pcSmallString, "%02X ", Fpu8Array[u16Index]);
+            strcat(FpcText, pcSmallString);
+        }
+        Logger_Append(FpcText);
     }
 }
 
--- Logger/Logger.h
+++ Logger/Logger.h
@@ -1,10 +1,13 @@
 #ifndef LOGGER_H_INCLUDED
 #define LOGGER_H_INCLUDED
 
+#define LOG_GROUP_BYTE_PER_LINE (32)
+
 extern FILE * LogStream;
 
 void Logger_Init(void);
-void Logger_Log(char* FpcString);
+void Logger_Append(char* FpcString);
 void Logger_Close(void);
+void Logger_AppendArray(char *FpcText, uint8_t *Fpu8Array, uint16_t Fu16Len);
 
 #endif // LOGGER_H_INCLUDED
